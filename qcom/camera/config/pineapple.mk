7b064e0ec69db0406fcc49973e9d9d58d733152f
raghavt20 <raghavt20@aospa.co>
2024-06-08 19:43:40 +0000

    devicetrees: camera: Add support for peridot

--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -5,7 +5,9 @@ dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
 									pineapple-camera-sensor-aim500.dtbo
+
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
 								cliffs-camera-sensor-mtp.dtbo \
-								cliffs-camera-sensor-qrd.dtbo
+								cliffs-camera-sensor-qrd.dtbo \
+								peridot-sm8635-camera-sensor.dtbo
